78fd969499a6008ad4b3b41eb8287ac7698d4d1e
Mikola Lysenko <mikolalysenko@gmail.com>
2014-03-30 18:25:52 -0500

    fixed bug with orientation check

--- rpis.js
+++ rpis.js
@@ -6,25 +6,23 @@ var sgn = require("signum")
 module.exports = inSimplex
 
 function inSimplex(simplex, point) {
-  var s = orientation.apply(undefined,simplex)
+  var s = sgn(orientation.apply(undefined,simplex))
   var scopy = simplex.slice()
   var boundary = false
-  var mind = Infinity
   for(var i=0; i<simplex.length; ++i) {
     scopy[i] = point
-    var o = orientation.apply(undefined,scopy)
+    var o = sgn(orientation.apply(undefined,scopy))
     scopy[i] = simplex[i]
     if(o) {
       if(sgn(o) !== s) {
-        return o
+        return -1
       }
     } else {
       boundary = true
     }
-    mind = Math.min(mind, o)
   }
   if(boundary) {
     return 0
   }
-  return mind
+  return 1
 }
--- test/test.js
+++ test/test.js
@@ -5,13 +5,25 @@ var sgn = require("signum")
 
 require("tape")(function(t) {
 
-  var triangle = [[0,0], [0,1], [1,0]]
-  t.equals(sgn(pointInSimplex(triangle, [0.25, 0.25])), 1)
-  t.equals(sgn(pointInSimplex(triangle, [0.5, 0.5])), 0)
-  t.equals(sgn(pointInSimplex(triangle, [-1, 0.5])), -1)
-  t.equals(sgn(pointInSimplex(triangle, [-1, -1])), -1)
-  t.equals(sgn(pointInSimplex(triangle, [0.5, -1])), -1)
-  t.equals(sgn(pointInSimplex(triangle, [1, 1])), -1)
+  function test(triangle) {
+    t.equals(sgn(pointInSimplex(triangle, [0.25, 0.25])), 1)
+    t.equals(sgn(pointInSimplex(triangle, [0.5, 0.5])), 0)
+    t.equals(sgn(pointInSimplex(triangle, [-1, 0.5])), -1)
+    t.equals(sgn(pointInSimplex(triangle, [-1, -1])), -1)
+    t.equals(sgn(pointInSimplex(triangle, [0.5, -1])), -1)
+    t.equals(sgn(pointInSimplex(triangle, [1, 1])), -1)
+
+
+    for(var i=0; i<3; ++i) {
+      t.equals(pointInSimplex(triangle, triangle[i]), 0)
+      var u = triangle[i]
+      var v = triangle[(i+1)%3]
+      t.equals(pointInSimplex(triangle, [0.5*(u[0]+v[0]), 0.5*(u[1]+v[1])]), 0)
+    }
+  }
+  test([[0,0], [0,1], [1,0]])
+  test([[0,0], [1,0], [0,1]])
+
 
   t.end()
 })
