4ed51e421c811ed0e321f2b349c4f45d58470aa8
Mikola Lysenko <mikolalysenko@gmail.com>
2014-05-06 20:43:17 -0500

    added more test cases

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 var pointInSimplex = require("../rpis.js")
 var sgn = require("signum")
 
-require("tape")(function(t) {
+require("tape")("point-in-simplex", function(t) {
 
   function test(triangle) {
     t.equals(sgn(pointInSimplex(triangle, [0.25, 0.25])), 1)
@@ -13,6 +13,10 @@ require("tape")(function(t) {
     t.equals(sgn(pointInSimplex(triangle, [0.5, -1])), -1)
     t.equals(sgn(pointInSimplex(triangle, [1, 1])), -1)
 
+    t.equals(pointInSimplex(triangle, [0, 10]), -1)
+    t.equals(pointInSimplex(triangle, [0, -10]), -1)
+    t.equals(pointInSimplex(triangle, [-10, 0]), -1)
+    t.equals(pointInSimplex(triangle, [10, 0]), -1)
 
     for(var i=0; i<3; ++i) {
       t.equals(pointInSimplex(triangle, triangle[i]), 0)
@@ -24,6 +28,13 @@ require("tape")(function(t) {
   test([[0,0], [0,1], [1,0]])
   test([[0,0], [1,0], [0,1]])
 
+
+
+  var tet = [[0,0,0], [1,0,0], [0,1,0], [0,0,1]]
+
+  t.equals(pointInSimplex(tet, [0.25, 0.25, 0.25]), 1)
+  t.equals(pointInSimplex(tet, [0.5, 0.5, 0.5]), -1)
+  t.equals(pointInSimplex(tet, [1, 1, 1]), -1)
 
   t.end()
 })
